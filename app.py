0350e35b8313020e06a9d9b7c33f7c73665cf864
zionhigt <larcher501@gmail.com>
2023-11-26 09:19:20 +0100

    Handle many file in exemple app

--- app.py
+++ app.py
@@ -7,14 +7,26 @@ def app(api=None):
 
             @api.post("/data")
             def data(req, res):
-                if getattr(req, 'body', None) is not None:
-                    print(req.body)
+                if req.body.get("file") is not None:
+                    data_file = req.body["file"].pop("data")
+
+                    with open("./" + req.body["file"]["filename"], "wb") as file:
+                        file.write(data_file)
+                if req.body.get("file_1") is not None:
+                    data_file_1 = req.body["file_1"].pop("data")
+
+                    with open("./" + req.body["file_1"]["filename"], "wb") as file_1:
+                        file_1.write(data_file_1)
                 res.with_status(201).sendjson({
                     "data-receive": req.body
                 })
 
+            @api.get("/redirect")
+            def redirect(req, res):
+                res.redirect("/public/9")
 
-            @api.get(["/public", "/public/:user"])
+
+            @api.get(["/public/:user"])
             @api.middleware("security.auth.check_user")
             def public(req, res):
                 user = req.params.get("user")
